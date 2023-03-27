e01ce86eab07e7291a5e556551c7bb55decbf3f0
VMG21 <A01411985@itesm.mx>
2023-03-27 16:29:59 -0600

    minor changes

--- src/main/java/com/vic/carrito/CarritoApplication.java
+++ src/main/java/com/vic/carrito/CarritoApplication.java
@@ -19,19 +19,19 @@ public class CarritoApplication implements CommandLineRunner {
 	@Autowired
 	private ItemRepository itemRepository;
 
-	BigDecimal a = new BigDecimal(45);
+	BigDecimal a = new BigDecimal(25);
 	BigDecimal b = new BigDecimal(45);
-	BigDecimal c = new BigDecimal(45);
+	BigDecimal c = new BigDecimal(10);
 
 	@Override
 	public void run(String... args) throws Exception {
-		Item item1 = new Item("asd", "asd", a);
+		Item item1 = new Item("Tomate", "tOmAtE", a);
 		itemRepository.save(item1);
 
-		Item item2 = new Item("asd", "asd", b);
+		Item item2 = new Item("Papa", "Papa Francisco", b);
 		itemRepository.save(item2);
 
-		Item item3 = new Item("asd", "asd", c);
+		Item item3 = new Item("Cebolla", "Te hace llorar", c);
 		itemRepository.save(item3);
 	}
 }
